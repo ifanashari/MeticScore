96a079604a2a1c489bd325a2d16d8bc10a3cc4ab
ifanashari <ifanashari1234@gmail.com>
2018-03-06 13:04:15 +0700

    Confirm pull

--- src/app/absensi/absensi.component.ts
+++ src/app/absensi/absensi.component.ts
@@ -12,7 +12,6 @@ export class AbsensiComponent implements OnInit {
   pesertaList: Peserta[];
   term:string;
   onStatus:boolean;
-  term:string;
   constructor(private fireService: DataService , private toastr: ToastrService) { }
 
   ngOnInit() {
